2cfe619444764f9824d2dfce15d63ffb87420926
FiachraCI <fiachraholland@gmail.com>
2022-05-13 18:55:53 +0000

    Wired up update button for saved recipes

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_instructions")
 def get_instructions():
+    '''Returns all recipes the user has saved'''
     instructions = list(mongo.db.instructions.find())
     return render_template("instructions.html", instructions=instructions)
 
 
 @app.route("/signup", methods=["GET", "POST"])
 def sign_up():
+    '''Allows new users to create an account'''
     if request.method == "POST":
         # check if username exists
         existing_user = mongo.db.users.find_one(
@@ -52,6 +54,7 @@ def sign_up():
 
 @app.route("/signin", methods=["GET", "POST"])
 def sign_in():
+    '''Allows existing users to sign in'''
     if request.method == "POST":
         # Checks if username already exisits in database
         existing_user = mongo.db.users.find_one(
@@ -81,7 +84,7 @@ def sign_in():
 
 @app.route("/account/<username>", methods=["GET", "POST"])
 def account(username):
-    # fetches the session user's username from the database
+    '''Fetches the session user's username from the database'''
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
@@ -93,6 +96,7 @@ def account(username):
 
 @app.route("/signout")
 def sign_out():
+    '''Allows user to sign out of their account'''
     flash("You have been signed out successfully")
     session.pop("user")
     return redirect(url_for("sign_in"))
@@ -100,6 +104,7 @@ def sign_out():
 
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    '''Allows logged in users to add a new recipe to their account'''
     if request.method == "POST":
         recipe = {
             "cuisine_name": request.form.get("cuisine_name"),
@@ -113,8 +118,18 @@ def add_recipe():
         mongo.db.instructions.insert_one(recipe)
         flash("Recipe added successfully!")
         return redirect(url_for("get_instructions"))
+
     cuisines = mongo.db.cuisine.find().sort("cuisine_name", 1)
     return render_template("add_recipe.html", cuisines=cuisines)
+
+
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    '''Allows logged in users to edit their saved recipes'''
+    recipe = mongo.db.instructions.find_one({"_id": ObjectId(recipe_id)})
+    cuisines = mongo.db.cuisine.find().sort("cuisine_name", 1)
+    return render_template(
+        "edit_recipe.html", recipe=recipe, cuisines=cuisines)
 
 
 if __name__ == "__main__":
